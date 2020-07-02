f951508d1fc288f0dd98b1a65fd3d9aa39dfcc79
GuidoAntoni <67267261+GuidoAntoni@users.noreply.github.com>
2020-07-02 16:38:42 +0200

    Add files via upload

---
+++ commandes.php
@@ -0,0 +1,31 @@
+<?php
+//tableau//
+
+$historique_commandes = array(5.49, 9.99, 5.49, 15.99, 25);
+
+
+
+//liste reçus//
+
+function afficher_commandes($historique_commandes)
+{
+    foreach($historique_commandes as $element)
+    echo 'Une commande de '.$element.' euros a été reçus.<br>';
+
+//calcul//
+
+    array_sum($historique_commandes);
+    echo "Le total de commande est " .array_sum($historique_commandes) . ' euros';
+}
+
+afficher_commandes($historique_commandes);
+
+
+
+
+
+
+
+
+
+?>
---
+++ produits.php
@@ -0,0 +1,29 @@
+<?php
+
+//Tableau//
+$produits = array(
+    'T-shirt rouge'=> 15.50,
+    'T-shirt vert'=> 15.50,
+    'T-shirt argent'=> 16.50,
+    'Short bleu'=> 19.99,
+    'Short ver'=> 19.99,
+    'Veste argent' => 35);
+
+    function afficher_produits($produits) 
+    {
+        echo'<table>
+        <tr>
+        <th>Produits</th>
+        <th>Prix</th>
+        </tr>';
+        foreach ($produits as $clef=> $value) 
+        {
+            echo'<tr>
+            <td>'.$clef.'</td>
+            <td>'.$value.'</td>
+            </tr>';
+        }
+    }
+
+    afficher_produits($produits)
+?>
